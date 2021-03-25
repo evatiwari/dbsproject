59e2aa307dab886341d8ea35f0dfe2d05e3ea4cc
Eva <eva.tiwari@gmail.com>
2021-03-25 20:07:03 +0530

    All tables added in db setup

--- database_setup.py
+++ database_setup.py
@@ -8,6 +8,24 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 #start inserting your tables below
+class User(Base):
+    __tablename__ = 'user'
+    user_id = Column(Integer, primary_key = True)
+    user_name =Column(String(80), nullable = False)
+    user_email=Column(String(80), nullable = False)
+    user_pass=Column(String(15),nullable = False)
+    user_phone=Column(String(10), nullable = False)
+    user_address=Column(String(100),nullable=False)
+    
+    
+class Trip(Base):
+    __tablename__ = 'trip'
+    trip_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    hotel_bookingnum=Column(Integer, ForeignKey('hotel_booking.booking_id'))
+    travel_bookingnum=Column(Integer, ForeignKey('transport_booking.booking_id'))
+    user=relationship(User)
+
 class Mode(Base):
     __tablename__='mode'
     mode_id=Column(Integer, primary_key=True)
@@ -15,7 +33,7 @@ class Mode(Base):
     price=Column(Integer)
 
 class TravelCompany(Base):
-	_tablename_ = 'travel_company'
+	__tablename__ = 'travel_company'
 	travel_name = Column(String(80), nullable = False)
 	travel_id = Column(Integer, primary_key = True)
 	travel_contact = Column(BigInteger)
@@ -33,13 +51,13 @@ class TransportBooking(Base):
     travel_id=Column(Integer, ForeignKey('travel_company.travel_id'))
 
 class Room(Base):
-	_tablename_ = 'room'
+	__tablename__ = 'room'
 	type_id = Column(Integer, primary_key= True)
 	room_type = Column(String(100) , nullable = False)
 	price = Column(BigInteger, nullable = False)
 
 class Hotel(Base):
-	_tablename_ = 'hotel'
+	__tablename__ = 'hotel'
 	hotel_name = Column(String(80), nullable = False)
 	hotel_id = Column(Integer, primary_key = True)
 	hotel_addr= Column(String(250))
@@ -47,7 +65,7 @@ class Hotel(Base):
 	hotel_num_room = Column(Integer)
 	
 class HotelA(Base):
-	_tablename_ = 'hotela'
+	__tablename__ = 'hotela'
 	hotela_id = Column(Integer,  ForeignKey('hotel.hotel_id') , primary_key = True)
 	room_no = Column(Integer, ForeignKey('room.type_id'), primary_key = True)
 	room = relationship(Room)
@@ -55,7 +73,7 @@ class HotelA(Base):
 
 
 class HotelBooking(Base) :
-	_tablename_ = 'hotel_booking'
+	__tablename__ = 'hotel_booking'
 	booking_id = Column(Integer, primary_key = True)
 	hotel_id = Column(Integer,  ForeignKey('hotel.hotel_id'))
 	check_in = Column(DateTime)
