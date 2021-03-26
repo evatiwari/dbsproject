b52aa884a113e5802b8d6568173149d88e7bf80c
Eva <eva.tiwari@gmail.com>
2021-03-27 00:56:36 +0530

    Added user login feature without SQL querying

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, session, render_template, request, redirect, g, url_for
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 from database_setup import Base, User, Trip, TransportBooking, TravelCompany,\
 Mode, Hotel, HotelA, HotelBooking, Room
 
 engine = create_engine('mysql+mysqlconnector://travel:dbmsproject@localhost:3306/sqlalchemy',echo=True)
 Base.metadata.create_all(engine)
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+app.secret_key = "ilikeskysowner"
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        session.pop('user', None)
+        #SQL query to retrieve password
+        if request.form['password'] == 'pass':
+            session['user'] = request.form['username']
+            return redirect(url_for('user'))
     return render_template("login.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
 
 @app.route('/user')
 def user():
-    return render_template("user.html")
+    if g.user:
+        return render_template("user.html", user=session['user'])
+    return redirect(url_for("login"))
+
+@app.before_request
+def before_request():
+    g.user=None
+    if 'user' in session:
+        g.user = session['user']
 
 @app.route('/hotel')
 def hotel():
